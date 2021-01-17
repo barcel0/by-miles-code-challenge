27a1d60b028965902ae4811726a46b56ef243b43
luis <lbarcelocarrera@gmail.com>
2021-01-17 09:30:55 +0100

    update title tag

--- src/screens/PolicyDetailsScreen.js
+++ src/screens/PolicyDetailsScreen.js
@@ -4,7 +4,6 @@ import LayoutMain from '../components/LayoutMain';
 import WhiteBox from '../components/WhiteBox';
 import ContentBlock from '../components/ContentBlock';
 import PolicyDetailsRow from '../components/PolicyDetailsRow';
-import LoadingSpinner from '../components/LoadingSpinner';
 
 const PolicyDetailsScreen = ({ policy, vehicle }) => {
   const { make, model, colour, reg } = vehicle;
